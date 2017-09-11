32e5c65cf434c383767451d03c3b0a1e601d06c6
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-11 12:18:48 +0200

    hello_others.py exercise done

---
+++ Week02/Day06/hello_others.py
@@ -0,0 +1,15 @@
+# Greet 3 of your classmates with this program, in three separate lines
+# like:
+#
+# Hello, Esther!
+# Hello, Mary!
+# Hello, Joe!
+
+# print('Hello, Esther!')
+# print('Hello, Mary!')
+# print('Hello, Joe!')
+
+#parentheses for python3
+names = ['Esther', 'Mary', 'Joe']
+for name in names:
+    print ('Hello, ' , name + '!')
