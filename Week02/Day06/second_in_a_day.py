d5f2049fe85e1c96bad71bfeff73667101048583
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-11 15:05:19 +0200

    seconds_in_a_day.py exercise corrected

--- Week02/Day06/second_in_a_day.py
+++ Week02/Day06/second_in_a_day.py
@@ -5,6 +5,6 @@ current_seconds = 42;
 # Write a program that prints the remaining seconds (as an integer) from a
 # day if the current time is represented bt the variables
 all_seconds = (24 * 60 * 60 )
-remaining_seconds = ( all_seconds - current_hours * 360 - current_minutes * 60 - current_seconds )
+remaining_seconds = ( all_seconds - current_hours * 3600 - current_minutes * 60 - current_seconds )
 
 print (remaining_seconds)
