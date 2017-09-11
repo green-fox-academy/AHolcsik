e14ab99e22cfc11765ae8175181a2f2172099dd1
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-11 14:25:10 +0200

    define_basic_info.py exercise done again

---
+++ Week02/Day06/define_basic_info.py
@@ -0,0 +1,12 @@
+# Define several things as a variable than print their values
+# Your name as a string
+# Your age as an integer
+# Your height in meters as a double
+# Wether you are married or not as a boolean
+
+name = ("Anna")
+age = 26
+height = 1.65
+is_married = False
+
+print(name, age, height, is_married)
