f91974f6e3b900f9c2d38fc9318d34416ead75be
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-11 15:29:44 +0200

    mile_to_km_converter.py exercise done

---
+++ Week02/Day06/mile_to_km_converter.py
@@ -0,0 +1,7 @@
+# Write a program that asks for an integer that is a distance in kilometers,
+# then it converts that value to miles and prints it
+
+converter = (0.6213)
+km = float(input ("Give me a distance in km! "))
+km *= converter
+print ("That distance is ", km, "in miles")
