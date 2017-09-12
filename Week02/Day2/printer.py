da53508d964934f593b1965362afad6e35b7ea8f
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-12 12:17:46 +0200

    feat(day2): printer.py exercise done

---
+++ Week02/Day2/printer.py
@@ -0,0 +1,9 @@
+# - Create a function called `printer`
+#   which prints the input parameters
+#   (can have multiple number of arguments)
+
+
+def printer(* anything):
+    print(anything)
+
+printer(23, 23, 42, 12, "b")
