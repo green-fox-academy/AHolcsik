5cdfefca7f4eb050612472269f6b6725baa61b1f
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-12 17:41:01 +0200

    feat(day2):colors.py exercise tried

--- Week02/Day2/colors.py
+++ Week02/Day2/colors.py
@@ -7,3 +7,7 @@
 # - In `colors[2]` store the shades of pink:
 #   `"orchid", "violet", "pink", "hot pink"`
 
+
+colors = [["lime", "forest green", "olive", "pale green", "spring green"], ["orange red", "red", "tomato"], ["orchid", "violet", "pink", "hot pink"]]
+
+print(colors)
