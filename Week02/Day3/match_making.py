6ea24b931a10792c72399a295318fcded6ca282f
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-13 14:06:05 +0200

    feat(day3): match_making.py exercise continued, too many names as result

--- Week02/Day3/match_making.py
+++ Week02/Day3/match_making.py
@@ -1,16 +1,20 @@
 # Join the two lists by matching one girl with one boy in the order list
 # Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
 
-girls = ["Eve", "Ashley", "Bözsi", "Kat", "Jane"]
-boys = ["Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"]
-order = []
+girls = ["Eve", "Ashley", "Bozsi", "Kat", "Jane"]
+boys = ["Joe", "Fred", "Bela", "Todd", "Neef", "Jeff"]
 
 def matching(girls, boys):
     order = []
-    for i in (girls):
-        order += i(girls), i(boys)
+    for i in (boys):
+        for j in (girls):
+            order.append (i)
+#        if i < len(boys):
+            
+            order.append (j)
+        else:
+            print (order)
+    return (order)
 
-
-    return order
-
-print(order)
+print(matching(girls,boys))
+  
