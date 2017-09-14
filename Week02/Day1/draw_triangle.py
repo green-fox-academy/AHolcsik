5aba852f5eb9be3f78941adeeb8d54519357c718
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-14 15:20:18 +0200

    feat(day4): draw_triangle.py exercise done (day1)

--- Week02/Day1/draw_triangle.py
+++ Week02/Day1/draw_triangle.py
@@ -8,12 +8,12 @@
 #
 # The triangle should have as many lines as the number was
 
-lines = input("How many lines should I draw? ")
+lines = int(input("How many lines should I draw? "))
 
 def draw_triangle(lines):
-    a = []
-    for i in lines:
-        a += "*"
-        print(a)
+    a = 1
+    while a <= lines:
+        print("*" * a)
+        a += 1
 
 draw_triangle(lines)
