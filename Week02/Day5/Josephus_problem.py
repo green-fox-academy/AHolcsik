f07a6577e5ee2343c374e5b2e1f195b7b2428626
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-15 12:29:50 +0200

    feat(Josephus_problem.py): exercise started, first idea written, second idea started

---
+++ Week02/Day5/Josephus_problem.py
@@ -0,0 +1,26 @@
+number_of_people = int(input("Hey, Josephus! What's the number of peeps? "))
+
+def solution(number_of_people):
+    remaining = number_of_people
+    while number_of_people != 1:
+        for i in (number_of_people):
+            remaining -= 1
+            
+
+
+def solution_again(number_of_people):
+    while number_of_people != 1:
+        if number_of_people 
+
+
+
+
+
+
+
+
+
+
+
+
+("Sit on the seat with number", number, "on it. You're welcome!")
