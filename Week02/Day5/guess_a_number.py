85f9e83211c8a31bc494ecb559b14b83fd23c463
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-15 10:33:55 +0200

    feat(guess_a_number.py): random number generator and custom range added

--- Week02/Day5/guess_a_number.py
+++ Week02/Day5/guess_a_number.py
@@ -1,4 +1,14 @@
 
+add_range_min, add_range_max = map(int, input("Give me the range of numbers (two numbers, split by space): ").split())
+
+
+import random
+for x in range(1):
+    x = (random.randint(1,101))
+
+#def guess()
+print (add_range_min, add_range_max)
+
 
 
 
