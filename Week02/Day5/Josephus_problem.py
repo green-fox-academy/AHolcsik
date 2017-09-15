53c0e58132033b9e633d6f2fca80393cbbb74241
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-15 14:36:34 +0200

    feat(Josephus_problem.py): idea not working, rethinking code

--- Week02/Day5/Josephus_problem.py
+++ Week02/Day5/Josephus_problem.py
@@ -1,27 +1,27 @@
 import math
 number_of_people = int(input("Hey, Josephus! What's the number of peeps? "))
 
-def solution(number_of_people):
-    remaining = number_of_people
-    while number_of_people != 1:
-        for i in (number_of_people):
-            remaining -= 1
+#def solution(number_of_people):
+#    remaining = number_of_people
+#    while number_of_people != 1:
+#        for i in (number_of_people):
+#            remaining -= 1
             
 
 
-def solution_again(number_of_people):
-    while number_of_people != 1:
-        if number_of_people is square root of two 
-            the result is one!
-        if number_of_people is not suare root of two
-            find highest number of power to 2 and decrease it
-            repeat for the rest until a remaining number is left 
-            #does this make sense?
-        the first power of two needs to be excluded
-        the number which "starts" then is the winner!
+#def solution_again(number_of_people):
+#    while number_of_people != 1:
+#        if number_of_people is square root of two 
+#            the result is one!
+#        if number_of_people is not suare root of two
+#            find highest number of power to 2 and decrease it
+#            repeat for the rest until a remaining number is left 
+#            #does this make sense?
+#        the first power of two needs to be excluded
+#        the number which "starts" then is the winner!
 
-if n = 2 ** number + l and l < 2*number
-winner is 2*l + 1
+#if n = 2 ** a + l and l < 2*number
+#winner is 2*l + 1
 
 #a is the power of 2 in the given number
 #find biggest power of a
@@ -29,17 +29,36 @@ winner is 2*l + 1
 
 
 
-math.sqrt(number_of_people)
+#def finding_square_root(number_of_people):
+#    if math.sqrt(number_of_people) % 2 == 0:
+#        print(math.sqrt(number_of_people))
+#        return (math.sqrt(number_of_people))
+#    else:
+#        number_of_people -= 1
+#        print(math.sqrt(number_of_people))
+#        return (number_of_people)
+
+
 #the power needs to be determined for formula
-def find_the_power(number_of_people):
-    return int(math.log(a, 2))
+#def find_the_power(number_of_people):
+#    return int(math.log(a, 2))
 
     
 
 
 
-def solution_final(a):
-    while number_of_people 
+#def solution_final(a):
+#    while number_of_people 
+
+#finding_square_root(number_of_people)
+#print(number_of_people)
+
+
+
+
+
+
+#in binary it's even easier but that's cheating
 
 
 
@@ -48,19 +67,10 @@ def solution_final(a):
 
 
 
-in binary it's even easier but that's cheating
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-("Sit on the seat with number", number, "on it. You're welcome!")
+#("Sit on the seat with number", number, "on it. You're welcome!")
