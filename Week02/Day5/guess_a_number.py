d91d0c80d15bdf159558919b14c330a422f77e49
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-15 10:45:56 +0200

    feat(guess_a_number.py): comparing numbers added and tested

--- Week02/Day5/guess_a_number.py
+++ Week02/Day5/guess_a_number.py
@@ -1,14 +1,24 @@
 
 add_range_min, add_range_max = map(int, input("Give me the range of numbers (two numbers, split by space): ").split())
 
-
 import random
 for x in range(1):
-    x = (random.randint(1,101))
+    x = (random.randint(add_range_min, add_range_max))
 
-#def guess()
+def compare_numbers(x):
+    user_guess = int(input("What is your guess? "))
+#    while user_guess != x:
+    if user_guess < x:
+        print("Too low! Guess again!")
+    elif user_guess > x:
+        print("Too high! Guess again!")
+    else:
+        print("Congrats! You guessed correctly! The number was ", x)
+    
+
+compare_numbers(x)
 print (add_range_min, add_range_max)
-
+print (x)
 
 
 
