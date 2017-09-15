628fe8416d486a6e31e55f492b3e4e19a14a22be
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-15 13:49:58 +0200

    feat(Josephus_problem.py): super messy notes

--- Week02/Day5/Josephus_problem.py
+++ Week02/Day5/Josephus_problem.py
@@ -10,7 +10,20 @@ def solution(number_of_people):
 
 def solution_again(number_of_people):
     while number_of_people != 1:
-        if number_of_people 
+        if number_of_people is square root of two 
+            the result is one!
+        if number_of_people is not suare root of two
+            find highest number of power to 2 and decrease it
+            repeat for the rest until a remaining number is left 
+            #does this make sense?
+        the first power of two needs to be excluded
+        the number which "starts" then is the winner!
+
+if n = 2^number + l and l < 2*number
+winner is 2*l + 1
+
+
+in binary it's even easier but that's cheating
 
 
 
