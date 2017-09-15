35b57fadf3e82f1dae73034c88a8e7633668e9cd
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-15 15:24:07 +0200

    feat(Josephus_problem.py): solved, some more cleaning

--- Week02/Day5/Josephus_problem.py
+++ Week02/Day5/Josephus_problem.py
@@ -6,8 +6,42 @@ number_of_people = int(input("Hey, Josephus! What's the number of peeps? "))
 #    while number_of_people != 1:
 #        for i in (number_of_people):
 #            remaining -= 1
-            
 
+#feed this number_of_people
+def highest_power_of_two(number_of_people):
+    if number_of_people < 0:
+       raise ValueError("Non-negative number only.")
+
+    if number_of_people == 0:
+       return 0
+
+    return 2 ** int(math.log(number_of_people, 2))
+
+rest = number_of_people - highest_power_of_two(number_of_people)
+#power = number_of_people - rest
+
+def winner_is(rest):
+    print("Sit on the seat with number", 2 * rest + 1, "on it. You're welcome!")
+    
+
+#the power needs to be determined for formula
+#def find_the_power(highest_power_of_two):
+#    return int(math.log(a, 2))
+
+#print(find_the_power)
+print(winner_is(rest))
+
+
+
+#def largestPowerOfTwoThatIsAFactorOf(number_of_people):
+#    if number_of_people % 2 != 0: return 1
+#    factor = 0
+#    while number_of_people % 2 == 0:
+#        number_of_people /= 2
+#        factor += 1
+#    return 2 ** factor
+
+#print(largestPowerOfTwoThatIsAFactorOf(number_of_people))
 
 #def solution_again(number_of_people):
 #    while number_of_people != 1:
@@ -28,20 +62,6 @@ number_of_people = int(input("Hey, Josephus! What's the number of peeps? "))
 
 
 
-
-#def finding_square_root(number_of_people):
-#    if math.sqrt(number_of_people) % 2 == 0:
-#        print(math.sqrt(number_of_people))
-#        return (math.sqrt(number_of_people))
-#    else:
-#        number_of_people -= 1
-#        print(math.sqrt(number_of_people))
-#        return (number_of_people)
-
-
-#the power needs to be determined for formula
-#def find_the_power(number_of_people):
-#    return int(math.log(a, 2))
 
     
 
