65b3b666851fa888941114adc5e75f993db1ff28
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-18 17:36:30 +0200

    feat(day1): encoded_lines.py exercise continued, trying idea

--- Week03/Day1/encoded_lines.py
+++ Week03/Day1/encoded_lines.py
@@ -1,12 +1,15 @@
 # Create a method that decrypts encoded-lines.txt
 
 file_name = "encoded_lines.txt"
+shift = 1
 
-def decrypt(file_name):
+def decrypt(file_name, shift):
+    alphabet = string.ascii_lowercase
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     f = open(file_name, "r")
     decrypted = ""
-    for element in f.readlines()[::-1]:
+    for element in f.read():
         decrypted += element
     print(decrypted)
 
-decrypt(file_name)
+decrypt(file_name, shift)
