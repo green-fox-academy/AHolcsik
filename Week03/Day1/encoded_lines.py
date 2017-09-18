20886408db866aa2492a02bd6071129d5a0bfaef
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-18 18:47:44 +0200

    feat(day1): encoded_lines.py exercise, failed try

--- Week03/Day1/encoded_lines.py
+++ Week03/Day1/encoded_lines.py
@@ -1,15 +1,33 @@
 # Create a method that decrypts encoded-lines.txt
 
+import string
+
 file_name = "encoded_lines.txt"
-shift = 1
 
-def decrypt(file_name, shift):
-    alphabet = string.ascii_lowercase
-    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+def caesar(file_name, shift):
     f = open(file_name, "r")
-    decrypted = ""
-    for element in f.read():
-        decrypted += element
-    print(decrypted)
+    shift %= 26
+    alphabet_lower = string.ascii_lowercase
+    alphabet_upper = string.ascii_uppercase
 
-decrypt(file_name, shift)
+    shifted_alphabet_lower = alphabet_lower[shift:] + alphabet_lower [:shift]
+    shifted_alphabet_upper = alphabet_upper[shift:] + alphabet_upper [:shift]
+
+    alphabet = alphabet_lower + alphabet_upper
+    shifted_alphabet = shifted_alphabet_lower + shifted_alphabet_upper
+
+    table = string.maketrans(alphabet, shifted_alphabet)
+
+    return plaintext.translate(table)
+
+#def decrypt(file_name):
+#    f = open(file_name, "r")
+#    decrypted = ""
+#    for element in f.read():
+#        for char in alphabet:
+#            if char == element:
+#                decrypted += char(-1)
+#    print(decrypted)
+
+#decrypt(file_name)
+print(caesar(file_name, 1))
