c6a463bda94f2e10ee56ad421178dded871dafbb
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-18 14:48:31 +0200

    feat(day1): copy_file.py exercise started, testing in other files

---
+++ Week03/Day1/copy_file.py
@@ -0,0 +1,13 @@
+# Write a function that copies a file to an other
+# It should take the filenames as parameters
+# It should return a boolean that shows if the copy was successful
+
+file_1 = "my-file.txt"
+file_2 = "new.txt"
+
+def copy_file(file_1, file_2):
+    f = open (file_1, "r")
+    fw = open (file_2, "r+")
+    for line in fw:
+        if "ROW" in line:
+            f.write(line)
