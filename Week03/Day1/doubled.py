ce9d68a36819f2a80e7f6be1d059fe8661b7dda9
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-18 16:49:18 +0200

    feat(day1): doubled.py exercise done

--- Week03/Day1/doubled.py
+++ Week03/Day1/doubled.py
@@ -4,10 +4,9 @@ file_name = "duplicated_chars.txt"
 
 def decrypt(file_name):
     f = open (file_name, "r")
-    decrypted = ()
-    for element in f.read():
-        if element != decrypted[-1]:
-            decrypted += element
+    decrypted = ""
+    for element in f.read()[::2]:
+         decrypted += element  
     print(decrypted)
 
 decrypt(file_name)
