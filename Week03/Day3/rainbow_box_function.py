a1b12729d213c0afebe7b78a0bd7a2d9acf2de11
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-20 13:55:36 +0200

    feat(day3): rainbow_box_function.py exercise done

--- Week03/Day3/rainbow_box_function.py
+++ Week03/Day3/rainbow_box_function.py
@@ -10,4 +10,20 @@ canvas.pack()
 # and draws a square of that size and color to the center of the canvas.
 # create a loop that fills the canvas with rainbow colored squares.
 
+square_1 = 20
+square_1_color = 'green'
+
+square_2 = 50
+square_2_color = 'purple'
+
+square_3 = 100
+square_3_color = 'blue'
+
+def center_square(x,y):
+    square = canvas.create_rectangle (150 - (x/2), 150 - (x/2), 150 + (x/2), 150 + (x/2), fill=y)
+
+center_square(square_3, square_3_color)
+center_square(square_2, square_2_color)
+center_square(square_1, square_1_color)
+
 root.mainloop()
