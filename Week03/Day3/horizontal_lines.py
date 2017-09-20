211ad4a38058ca12d65f87eb9482bac6d2fbb635
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-20 12:16:35 +0200

    feat(day3): horizontal_lines.py exercise done

--- Week03/Day3/horizontal_lines.py
+++ Week03/Day3/horizontal_lines.py
@@ -10,4 +10,22 @@ canvas.pack()
 # and draws a 50 long horizontal line from that point.
 # draw 3 lines with that function.
 
+x1 = 43
+y1 = 170
+
+x2 = 90
+y2 = 300
+
+x3 = 189
+y3 = 46
+
+def lines_to_center(x, y):
+    new_line = canvas.create_line(x, y, (x + 50), y)
+
+lines_to_center(x1,y1)
+lines_to_center(x2,y2)
+lines_to_center(x3,y3)
+root.mainloop()
+
+
 root.mainloop()
