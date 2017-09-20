d37a2765aab5affa80bd86c91af3ec9e0f695b4b
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-20 17:25:52 +0200

    feat(day3): line_play_quarters.py exercise done

--- Week03/Day3/line_play_quarters.py
+++ Week03/Day3/line_play_quarters.py
@@ -9,4 +9,28 @@ canvas.pack()
 # and repeat this pattern in each quarter:
 # [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
 
+steps = 10
+
+def purple_web(steps, x_start, y_start, x_end, y_end):
+    for i in range(16):
+        canvas.create_line(x_start, y_start, x_end, y_end, fill='purple')
+        x_start += steps
+        y_end += steps
+
+def green_web(steps, x_start, y_start, x_end, y_end):
+    for i in range(16):
+        canvas.create_line(x_start, y_start, x_end, y_end, fill='green')
+        y_start += steps
+        x_end += steps
+
+purple_web(steps, 0, 0, 150, 0)
+purple_web(steps, 150, 0, 300, 0)
+purple_web(steps, 0, 150, 150, 150)
+purple_web(steps, 150, 150, 300, 150)
+
+green_web(steps, 0, 0, 0, 150)
+green_web(steps, 150, 0, 150, 150)
+green_web(steps, 0, 150, 0, 300)
+green_web(steps, 150, 150, 150, 300)
+
 root.mainloop()
