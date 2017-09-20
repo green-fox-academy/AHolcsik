03892433c944f0b1b226815ecd1e8e579cd03afa
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-20 11:29:16 +0200

    feat(day3): setting up environmnet

---
+++ Week03/Day3/line_in_the_middle.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+# draw a red horizontal line to the canvas' middle.
+# draw a green vertical line to the canvas' middle.
+
+root.mainloop()
