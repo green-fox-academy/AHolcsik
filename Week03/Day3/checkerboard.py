dea09a921be5759a8a01e92c04a8d46d476774d1
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-20 16:25:13 +0200

    feat(day3): checkerboard.py exercise done

--- Week03/Day3/checkerboard.py
+++ Week03/Day3/checkerboard.py
@@ -7,4 +7,19 @@ canvas.pack()
 
 # fill the canvas with a checkerboard pattern.
 
+board_width = 8
+
+def checkerboard(board_width):
+    size = 30
+    x = 30
+    y = 30
+    for i in range(board_width):
+        for j in range(board_width):
+            if (i + j) % 2 == 0:
+                tile = canvas.create_rectangle(x + (i * size), y + (j * size), 2 * x + (i * size), 2 * y + (j * size), fill='black')
+            else:
+                tile = canvas.create_rectangle(x + (i * size), y + (j * size), 2 * x + (i * size), 2 * y + (j * size), fill='white')
+
+checkerboard(board_width)
+
 root.mainloop()
