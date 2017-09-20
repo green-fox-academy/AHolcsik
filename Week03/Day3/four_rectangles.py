b274a4afd8de3a06b0ed9078a90646b1e375cb16
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-20 12:26:43 +0200

    feat(day3): four_rectangles.py exercise done

--- Week03/Day3/four_rectangles.py
+++ Week03/Day3/four_rectangles.py
@@ -7,4 +7,9 @@ canvas.pack()
 
 # draw four different size and color rectangles.
 
+green_box = canvas.create_rectangle(23, 45, 79, 102, fill='green')
+pink_box = canvas.create_rectangle(223, 75, 290, 2, fill='pink')
+yellow_box = canvas.create_rectangle(178, 5, 159, 207, fill='yellow')
+blue_box = canvas.create_rectangle(30, 245, 149, 190, fill='blue')
+
 root.mainloop()
