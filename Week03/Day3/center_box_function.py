dc2cefb0a2e7a9efd9f664e24ee91dc8f35b08e6
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-20 13:52:04 +0200

    feat(day3): center_box_function.py exercise done

--- Week03/Day3/center_box_function.py
+++ Week03/Day3/center_box_function.py
@@ -10,4 +10,15 @@ canvas.pack()
 # and draws a square of that size to the center of the canvas.
 # draw 3 squares with that function.
 
+square_1 = 20
+square_2 = 50
+square_3 = 100
+
+def center_square(x):
+    square = canvas.create_rectangle (150 - (x/2), 150 - (x/2), 150 + (x/2), 150 + (x/2))
+
+center_square(square_1)
+center_square(square_2)
+center_square(square_3)
+
 root.mainloop()
