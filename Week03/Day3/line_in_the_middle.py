8079e04f73c054066ee4361867b271669d8d4837
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-20 11:34:48 +0200

    feat(day3): line_in_the_middle.py exercise done

--- Week03/Day3/line_in_the_middle.py
+++ Week03/Day3/line_in_the_middle.py
@@ -8,4 +8,7 @@ canvas.pack()
 # draw a red horizontal line to the canvas' middle.
 # draw a green vertical line to the canvas' middle.
 
+redline = canvas.create_line(0, 150, 300, 150, fill = 'tomato')
+greenline = canvas.create_line(150, 0, 150, 300, fill = 'sea green')
+
 root.mainloop()
