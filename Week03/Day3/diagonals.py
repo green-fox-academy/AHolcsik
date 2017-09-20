23732d6d7ffeb7693ac4ce53a66fc79e17f1f7f9
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-20 11:47:54 +0200

    feat(day3): diagonals.py exercise done

--- Week03/Day3/diagonals.py
+++ Week03/Day3/diagonals.py
@@ -7,4 +7,7 @@ canvas.pack()
 
 # draw the canvas' diagonals in green.
 
+left_diagonal = canvas.create_line(0, 0, 300, 300, fill='green')
+right_diagonal = canvas.create_line(300, 0, 0, 300, fill='green')
+
 root.mainloop()
