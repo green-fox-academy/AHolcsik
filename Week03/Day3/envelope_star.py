b6c746f5088e2ecb188e3d80fab6fd6e29d3f7ae
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-20 21:34:24 +0200

    feat(day3): envelope_star.py exercise started

--- Week03/Day3/envelope_star.py
+++ Week03/Day3/envelope_star.py
@@ -8,4 +8,36 @@ canvas.pack()
 # reproduce this:
 # [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/envelope-star/r2.png]
 
+steps = 10
+
+def upper_left_web(steps, x_start, y_start, x_end, y_end):
+    for i in range(16):
+        canvas.create_line(x_start, y_start, x_end, y_end, fill='purple')
+        x_start += steps
+        y_end += steps
+
+def upper_right_web(steps, x_start, y_start, x_end, y_end):
+    for i in range(16):
+        canvas.create_line(x_start, y_start, x_end, y_end, fill='purple')
+        x_start += steps
+        y_end += steps
+
+def lower_left_web(steps, x_start, y_start, x_end, y_end):
+    for i in range(16):
+        canvas.create_line(x_start, y_start, x_end, y_end, fill='green')
+        y_start += steps
+        x_end += steps
+
+def lower_right_web(steps, x_start, y_start, x_end, y_end):
+    for i in range(16):
+        canvas.create_line(x_start, y_start, x_end, y_end, fill='green')
+        y_start += steps
+        x_end += steps
+
+upper_left_web(steps, 0, 0, 150, 0)
+upper_right_web(steps, 0, 150, 150, 150)
+lower_left_web(steps, 0, 150, 150, 150)
+lower_right_web(steps, 150, 300, 300, 150)
+
+
 root.mainloop()
