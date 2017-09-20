e9f98f0de87049b436c1ad8ab0b0636b316f8fb9
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-20 12:30:59 +0200

    feat(day3): position_square.py exercise done

--- Week03/Day3/position_square.py
+++ Week03/Day3/position_square.py
@@ -10,4 +10,26 @@ canvas.pack()
 # and draws a 50x50 square from that point.
 # draw 3 squares with that function.
 
+x1 = 43
+y1 = 170
+
+x2 = 90
+y2 = 200
+
+x3 = 189
+y3 = 46
+
+def square(x, y):
+    new_square = canvas.create_rectangle(x, y, (x + 50), (y + 50))
+
+square(x1,y1)
+square(x2,y2)
+square(x3,y3)
+
+root.mainloop()
+
+
+
+
+
 root.mainloop()
