f6ce4d1afa2d183bba5a147e6941611ae9eb6b9e
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-20 11:45:40 +0200

    feat(day3): colored_box.py exercise done

--- Week03/Day3/colored_box.py
+++ Week03/Day3/colored_box.py
@@ -7,4 +7,9 @@ canvas.pack()
 
 # draw a box that has different colored lines on each edge.
 
+green_line = canvas.create_line(30, 30, 130, 30, fill = 'green')
+blue_line = canvas.create_line(130, 30, 130, 130, fill = 'blue')
+purple_line = canvas.create_line(130, 130, 30, 130, fill = 'purple')
+orange_line = canvas.create_line(30, 130, 30, 30, fill = 'orange')
+
 root.mainloop()
