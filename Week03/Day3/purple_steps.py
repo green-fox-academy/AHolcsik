0855fa630d8d1fa30b36a0e74d744cded3b45a62
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-20 14:30:17 +0200

    feat(day3): purple_steps.py exercise done

--- Week03/Day3/purple_steps.py
+++ Week03/Day3/purple_steps.py
@@ -2,10 +2,22 @@ from tkinter import *
 
 root = Tk()
 
-canvas = Canvas(root, width='300', height='300')
+canvas = Canvas(root)
 canvas.pack()
+canvas_width = 300
+canvas_height = 300
 
 # reproduce this:
 # [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+
+
+nsteps = 20
+step_x = int(canvas_width / nsteps)
+step_y = int(canvas_height / nsteps)
+
+for i in range(1, nsteps):
+    purple_square = canvas.create_rectangle(0+(i*10), 0+(i*10), 10+(i*10), 10+(i*10), fill='purple')
+
+
 
 root.mainloop()
