a44f2c1da6c11ff4fad63c440ab83bf0baef432f
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-21 11:59:44 +0200

    feat(day4): string1.py exercise done

--- Week03/Day4/string1.py
+++ Week03/Day4/string1.py
@@ -1,2 +1,15 @@
 # Given a string, compute recursively (no loops) a new string where all the
 # lowercase 'x' chars have been changed to 'y' chars.
+
+old = 'x'
+new = 'y'
+
+def x_to_y(string, old, new):
+    if string == '':
+        return ''
+    elif string[:len(old)] == old:
+        return new + x_to_y(string[len(old):], old, new)
+    else:
+        return string[0] + x_to_y(string[1:], old, new)
+
+print(x_to_y('xanadu', old, new))
