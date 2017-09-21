783951bff02e9c52b424ef0544df450858d54228
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-21 16:05:57 +0200

    feat(day4): board.py exercise continued, coordinates added

--- Week03/Day4/board.py
+++ Week03/Day4/board.py
@@ -5,13 +5,17 @@ root = Tk()
 canvas = Canvas(root, width='500', height='500')
 canvas.pack()
 
-size = 300
-
 def squares(size):
-    if size < 10:
-        return
+    x = 0
+    y = 0
+    if size > 10:
+        return 0
     else: 
-        canvas.create_rectangle(100, 100, 100 + size, 100 + size)
+        canvas.create_line(x + size/3, y, x + size/3, y + size)
+        canvas.create_line(x + size/3*2, y, x + size/3*2, y + size)
+        canvas.create_line(x, x + size/3, x + size, y + size/3)
+        canvas.create_line(x, x + size/3*2, x + size, y + size/3*2)
+        return (  )
 
 print(squares(size))
 
