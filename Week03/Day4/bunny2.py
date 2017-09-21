cbc75e10f7fea486cea70bd35a62695e09dbdd50
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-21 11:24:26 +0200

    feat(day4): bunny2.py exercise done

--- Week03/Day4/bunny2.py
+++ Week03/Day4/bunny2.py
@@ -2,3 +2,14 @@
 # (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
 # have 3 ears, because they each have a raised foot. Recursively return the
 # number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+def appendeges_o_bunnies(num):
+    if num == 1:
+        return 2
+    elif num % 2 == 0:
+        return 3 + (appendeges_o_bunnies(num-1))
+    else:
+        return 2 + (appendeges_o_bunnies(num-1))
+
+print(appendeges_o_bunnies(3))
+
