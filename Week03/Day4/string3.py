ebca1dc8fd52c221a0c68305ff3ad3774f4ee607
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-21 12:19:58 +0200

    feat(day4): string3.py exercise done

--- Week03/Day4/string3.py
+++ Week03/Day4/string3.py
@@ -1,2 +1,15 @@
 # Given a string, compute recursively a new string where all the
-# adjacent chars are now separated by a "*".
+# adjacent chars are now separated by a "*".old = 'x'
+# qwer => q*w*e*r
+
+new = '*'
+
+def x_to_y(string, new):
+    if string == '':
+        return ''
+    elif len(string) == 1:
+        return string [0]
+    else:
+        return string[0] + new + x_to_y(string[1:], new)
+
+print(x_to_y('xanadu', new))
