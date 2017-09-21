dbcabe8b994f0471ad32fa60055972c6e36e4f62
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-21 12:01:23 +0200

    feat(day4): string2.py exercise done

--- Week03/Day4/string2.py
+++ Week03/Day4/string2.py
@@ -1,1 +1,15 @@
-# Given a string, compute recursively a new string where all the 'x' chars have been removed.
+# Given a string, compute recursively a new
+# string where all the 'x' chars have been removed.
+
+old = 'x'
+new = ''
+
+def x_to_y(string, old, new):
+    if string == '':
+        return ''
+    elif string[:len(old)] == old:
+        return new + x_to_y(string[len(old):], old, new)
+    else:
+        return string[0] + x_to_y(string[1:], old, new)
+
+print(x_to_y('xanadu', old, new))
