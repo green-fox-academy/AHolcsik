9c0cf513239f2c9fdfbeb59c1cb849cde8b24d9a
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-22 13:46:41 +0200

    feat(day4): recursion_to_center.py exercise started, basic structure made

--- Week03/Day3/function_to_center.py
+++ Week03/Day3/function_to_center.py
@@ -10,4 +10,18 @@ canvas.pack()
 # and draws a line from that point to the center of the canvas.
 # fill the canvas with lines from the edges, every 20 px, to the center.
 
+def draw_line(x,y):
+    canvas.create_line(x, y, x + 150, y + 150, fill = 'black')
+
+def go_to_center(x, y, run):
+    if run == 0:
+        return
+    else:
+        draw_line(x,y)
+        canvas.create_line(x+10, y, x + 150, y + 150, fill = 'black')
+        canvas.create_line(x, y+10, x + 150, y + 150, fill = 'black')
+        
+
+go_to_center(0, 0, 100)
+
 root.mainloop()
