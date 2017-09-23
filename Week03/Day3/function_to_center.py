802c1bad3c1be5d0436e112764745a56f3048d2d
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-23 18:48:48 +0200

    feat(day4): recursion_to_center.py exercise done, time.sleep added

--- Week03/Day3/function_to_center.py
+++ Week03/Day3/function_to_center.py
@@ -1,9 +1,11 @@
 from tkinter import *
+import time
 
 root = Tk()
 
 canvas = Canvas(root, width='300', height='300')
 canvas.pack()
+
 
 # create a line drawing function that takes 2 parameters:
 # the x and y coordinates of the line's starting point
@@ -11,6 +13,8 @@ canvas.pack()
 # fill the canvas with lines from the edges, every 20 px, to the center.
 
 def draw_line(x, y):
+    time.sleep(0.1)
+    canvas.update()
     canvas.create_line(x, y, 150, 150, fill = 'black')
 
 def go_to_center_x(x, y):
@@ -19,14 +23,14 @@ def go_to_center_x(x, y):
     else:
         draw_line(x,y)
         go_to_center_x(x + 10, y)
-
+        
 
 def go_to_center_y(x, y):
-    if y == 300:
+    if y == 0:
         return
     else:
         draw_line(x,y)
-        go_to_center_y(x, y + 10)
+        go_to_center_y(x, y - 10)
 
 
 def come_back_x(x, y, run):
@@ -37,18 +41,15 @@ def come_back_x(x, y, run):
         come_back_x(x - 10, y, run -1)
 
 def come_back_y(x, y):
-    if y == -10:
+    if y == 300:
         return
     else:
         draw_line(x,y)
-        come_back_y(x, y - 10)
-
-come_back_x(300, 300, 31)
-come_back_y(300, 300)
-
+        come_back_y(x, y + 10)
 
 go_to_center_x(0, 0)
-go_to_center_y(0, 0)
-
+come_back_y(300, 0)
+come_back_x(300, 300, 30)
+go_to_center_y(0, 300)
 
 root.mainloop()
