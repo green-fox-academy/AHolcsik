49d8fdc30c1fc405b1b4e0b06ece742865224d39
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-23 13:24:32 +0200

    feat(day4): recursion_to_center.py exercise done, different variants for base case

--- Week03/Day3/function_to_center.py
+++ Week03/Day3/function_to_center.py
@@ -10,18 +10,45 @@ canvas.pack()
 # and draws a line from that point to the center of the canvas.
 # fill the canvas with lines from the edges, every 20 px, to the center.
 
-def draw_line(x,y):
-    canvas.create_line(x, y, x + 150, y + 150, fill = 'black')
+def draw_line(x, y):
+    canvas.create_line(x, y, 150, 150, fill = 'black')
 
-def go_to_center(x, y, run):
+def go_to_center_x(x, y):
+    if x == 300:
+        return
+    else:
+        draw_line(x,y)
+        go_to_center_x(x + 10, y)
+
+
+def go_to_center_y(x, y):
+    if y == 300:
+        return
+    else:
+        draw_line(x,y)
+        go_to_center_y(x, y + 10)
+
+
+def come_back_x(x, y, run):
     if run == 0:
         return
     else:
         draw_line(x,y)
-        canvas.create_line(x+10, y, x + 150, y + 150, fill = 'black')
-        canvas.create_line(x, y+10, x + 150, y + 150, fill = 'black')
-        
+        come_back_x(x - 10, y, run -1)
 
-go_to_center(0, 0, 100)
+def come_back_y(x, y):
+    if y == -10:
+        return
+    else:
+        draw_line(x,y)
+        come_back_y(x, y - 10)
+
+come_back_x(300, 300, 31)
+come_back_y(300, 300)
+
+
+go_to_center_x(0, 0)
+go_to_center_y(0, 0)
+
 
 root.mainloop()
