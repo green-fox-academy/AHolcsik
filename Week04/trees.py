31064438f1335126cf34cbe69b7e4f511cb1c69b
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-25 10:06:30 +0200

    feat(week04): homework something

---
+++ Week04/trees.py
@@ -0,0 +1,12 @@
+
+trees = [
+    {tree_type: 'cedar', leaf_color: 'grey', age: '11', sex: 'monoecious'}
+    {tree_type: 'ginkgo', leaf_color: 'light green', age: '57', sex: 'female'}
+    {tree_type: 'yew', leaf_color: 'blue', age: '22', sex: 'female'}
+    {tree_type: 'mulberry', leaf_color: 'green', age: '12', sex: 'male'}
+    {tree_type: 'sweetgum', leaf_color: 'red', age: '33', sex: 'monoecious'}
+]
+
+def tree_caller(trees):
+    for key, value in trees.items():
+        return value
