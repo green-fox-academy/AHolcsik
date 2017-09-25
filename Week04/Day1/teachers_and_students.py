b47b87799b907a1e9eac5c1aafb8c63a3763d2dd
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-25 17:09:43 +0200

    feat(day1): teachers_and_students.py exercise continued

--- Week04/Day1/teachers_and_students.py
+++ Week04/Day1/teachers_and_students.py
@@ -1,7 +1,21 @@
-Create Student and Teacher classes
-Student
-learn()
-question(teacher) -> calls the teachers answer method
-Teacher
-teach(student) -> calls the students learn method
-answer()
+# Create Student and Teacher classes
+# Student
+# learn()
+# question(teacher) -> calls the teachers answer method
+# Teacher
+# teach(student) -> calls the students learn method
+# answer()
+
+class Student(object):
+
+    def learn():
+
+    def question(teacher):
+        teacher.teach
+
+
+class Teacher(object):
+
+    def teach(student):
+        student.learn
+
