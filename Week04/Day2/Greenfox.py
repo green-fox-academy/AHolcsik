de0ab16e14e44addc455b82b396a9c6bca2b72ff
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-26 12:24:54 +0200

    feat(day2): greenfox.py exercise continued, person and student classes added

--- Week04/Day2/Greenfox.py
+++ Week04/Day2/Greenfox.py
@@ -5,26 +5,34 @@ class Person:
         self.age = int(age)
         self.gender = gender
 
-    def introduce():
-        print("Hi, I'm {}, a {} year old {} gender".format(self.name, self.age, self.gender))
+    def introduce(self):
+        print("Hi, I'm {}, a {} year old {}".format(self.name, self.age, self.gender))
 
-    def get_goal():
+    def get_goal(self):
         print("My goal is: Live for the moment!")
 
 class Student(Person):
 
-    def __init__(self, previous_organization = "Shool of Life", skipped_days = 0):
+    def __init__(self, name = "Jane Doe", age = "30", gender = "female", previous_organization = "The Shool of Life", skipped_days = 0):
+        super().__init__()
+        self.name = name
+        self.age = int(age)
+        self.gender = gender
         self.previous_organization = previous_organization
         self.skipped_days = skipped_days
 
-    def get_goal():
+    def get_goal(self):
         print("Be a junior software developer.") 
 
-    def introduce():
-        print("Hi, I'm {}, a {} year old {} from {} who skipped {} days from the course already.").format(self.name, self.age, self.gender, self.previous_organization, self.skipped_days)
+    def introduce(self):
+        print("Hi, I'm {}, a {} year old {} from {} who skipped {} days from the course already.".format(self.name, self.age, self.gender, self.previous_organization, self.skipped_days))
 
     def skipped_days(number_of_days):
         self.skipped_days += number_of_days
+
+
+
+
 
 stu = Student()
 stu.introduce()
