7461c87f7a02cb3a00986f41697fcf9bfb35d575
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-27 18:05:24 +0200

    feat(day3): extend.py and test.py started, done until vovels exercise

---
+++ Week04/Day3/extend.py
@@ -0,0 +1,40 @@
+
+# Adds a and b, returns as result
+def add(a, b):
+    return a + b
+
+# Returns the highest value from the three given params
+def max_of_three(a, b, c):
+    max = a
+    if b > max:
+        max = b
+    if c > max:
+        max = c
+    return max
+
+# Returns the median value of a list given as param
+def median(pool):
+    n = len(pool)
+    if n < 1:
+        return None
+    if n % 2 == 1:
+        return sorted(pool)[n//2]
+    else:
+        return sum(sorted(pool)[n//2-1:n//2+1])/2.0
+
+# Returns true if the param is a vovel
+def is_vovel(char):
+    vovels = 'aeiouéáőűöüóí'
+    output = ''
+    for letter in char:
+        if letter in vovels:
+            output += letter
+    return output
+
+# Create a method that translates hungarian into the teve language
+def translate(hungarian):
+    teve = hungarian
+    for char in teve:
+        if is_vovel(char):
+            teve = (char+'v'+char).join(teve.split(char))
+    return teve
---
+++ Week04/Day3/test.py
@@ -0,0 +1,45 @@
+import unittest
+import extend
+
+class TestExtend(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_add_2_and_3_is_5(self):
+        self.assertEqual(extend.add(2, 3), 5)
+
+    def test_add_4_and_1_is_5(self):
+        self.assertEqual(extend.add(3, 1), 4)
+
+    def test_max_of_three_first(self):
+        self.assertEqual(extend.max_of_three(4, 2, 3), 4)
+
+    def test_max_of_three_second(self):
+        self.assertEqual(extend.max_of_three(1, 4, 3), 4)
+
+    def test_max_of_three_third(self):
+        self.assertEqual(extend.max_of_three(3, 4, 5), 5)
+
+    def test_median_four_a(self):
+        self.assertEqual(extend.median([7,5,3,5]), 5)
+
+    def test_median_four_b(self):
+        self.assertEqual(extend.median([2,1,5,2]), 2)
+
+    def test_median_five(self):
+        self.assertEqual(extend.median([4,1,2,3,5]), 3)
+
+    def test_is_vovel_a(self):
+        self.assertTrue(extend.is_vovel('au'))
+
+    def test_is_vovel_u(self):
+        self.assertTrue(extend.is_vovel('u'))
+
+    def test_translate_bemutatkozik(self):
+        self.assertEqual(extend.translate('bemutatkozik'), 'bevemuvutavatkovozivik')
+
+    def test_translate_kolbice(self):
+        self.assertEqual(extend.translate('kolbice'), 'kovolbiviceve')
+
+if __name__ == '__main__':
+    unittest.main()
