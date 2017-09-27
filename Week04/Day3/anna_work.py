5f4a958d93aaee89968602d81d3a118caf60f114
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-27 16:01:04 +0200

    feat(day3): counter exercise started, test names changed in anagramm test

--- Week04/Day3/anna_work.py
+++ Week04/Day3/anna_work.py
@@ -58,12 +58,11 @@ class Anagramm:
                     return True
         return False
 
+# Write a function, that takes a string as an argument and returns a dictionary with all letters in the string as keys, and numbers as values that shows how many occurrences there are.
+# Create a test for that.
 
+class CountLetters:
 
-
-
-
-
-
-
+    def number_of_letters(self, string):
+        return
 
--- Week04/Day3/test_anna_work.py
+++ Week04/Day3/test_anna_work.py
@@ -1,14 +1,15 @@
 import unittest
-from anna_work import GetsApple, SumExercise, Anagramm
+from anna_work import GetsApple, SumExercise, Anagramm, CountLetters
 
 test_apple = GetsApple()
 test_valami = SumExercise()
 test_anagramm = Anagramm()
+test_word = CountLetters
 
 class GetAppleTest(unittest.TestCase):
 
     def test_filled(self):
-        self.assertEqual(test_apple.get_apple(), "apple")
+        self.assertEqual(test_apple.get_apple(), 'apple')
 
 class SumExerciseTest(unittest.TestCase):
 
@@ -23,17 +24,24 @@ class SumExerciseTest(unittest.TestCase):
 
 class AnagrammTest(unittest.TestCase):
 
-    def test_is_working(self):
+    def test_if_anagramm(self):
         self.assertTrue(test_anagramm.is_anagramm('dog','god'))
 
-    def test_is_working(self):
+    def test_if_letter_number_is_equal(self):
         self.assertFalse(test_anagramm.is_anagramm('purple','mapple'))
 
-    def test_is_working(self):
+    def test_if_letter_number_is_different(self):
         self.assertFalse(test_anagramm.is_anagramm('hello','helloworld'))
 
-    def test_is_working(self):
+    def test_if_strings_are_same(self):
         self.assertTrue(test_anagramm.is_anagramm('doggo','doggo'))
+
+class CountLettersTest(unittest.TestCase):
+
+    def test_for_empty(self):
+        self.assertTrue(test_word.number_of_letters(''), {})
+
+
 
 if __name__ == '__main__':
     unittest.main()
