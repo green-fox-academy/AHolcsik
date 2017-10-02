62015b5029c434c7909cf8edcab8b7d11a1ee3fa
AHolcsik <annamaria.holcsik@gmail.com>
2017-10-02 16:31:40 +0200

    feat(RPG): map added with floors and walls

--- Week05/RPG/main.py
+++ Week05/RPG/main.py
@@ -2,14 +2,39 @@ from tkinter import *
 
 
 root = Tk()
-canvas = Canvas(root, width = 1000, height = 1000)
+canvas = Canvas(root, width = 800, height = 800)
 canvas.pack()
 
 floor = PhotoImage(file = 'floor.gif')
-canvas.create_image(10, 10, image = floor)
+wall = PhotoImage(file = 'wall.gif')
 
+def floor_piece(x, y, size):
+    floo = canvas.create_image(x * size, y * size, image = floor)
 
+def wall_piece(x, y, size):
+    woo = canvas.create_image(x * size, y * size, image = wall)
 
+def draw_whole_map(map_layout, x, y, size):
+    for column in range(len(map_layout)):
+        for row in range(len(map_layout[0])):
+            if map_layout[column][row] == 0:
+                floor_piece(row + 0.6, column + 0.6, size)
+            elif map_layout[column][row] == 1:
+                wall_piece(row + 0.6, column + 0.6, size)
 
+map_layout = [[0,0,0,1,0,1,0,0,0,0],
+              [0,0,0,1,0,1,0,1,1,0],
+              [0,1,1,1,0,1,0,1,1,0],
+              [0,0,0,0,0,1,0,0,0,0],
+              [1,1,1,1,0,1,1,1,1,0],
+              [0,1,0,1,0,0,0,0,1,0],
+              [0,1,0,1,0,1,1,0,1,0],
+              [0,0,0,0,0,1,1,0,1,0],
+              [0,1,1,1,0,0,0,0,1,0],
+              [0,0,0,1,0,1,1,0,1,0],
+              [0,1,0,1,0,1,0,0,0,0]
+              ]
+
+draw_whole_map(map_layout, 72, 72, 72)
 
 root.mainloop()
