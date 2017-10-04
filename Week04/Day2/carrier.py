8f7e2a05d5f11439101c01a2dd9d7eb41ceceec7
AHolcsik <annamaria.holcsik@gmail.com>
2017-10-04 14:45:36 +0200

    feat(aircraft): function calling from carrier does not work well, needs fixing and refractoring

--- Week04/Day2/carrier.py
+++ Week04/Day2/carrier.py
@@ -1,8 +1,25 @@
 from aircraft import Aircraft, F16, F35
 
-class Carrier(Object):
+class Carrier():
 
-    def __init__(self):
+    def __init__(self, ammo_stored = 600, health_point = 500):
+        self.stored_crafts = []
+        self.ammo_stored = ammo_stored
+        self.health_point = health_point
+
+    def add_aircraft(self, aircraft_type):
+        return self.stored_crafts(aircraft_type.status(aircraft_type))
+
+
+        # if x == aircraft_type(x):
+        #     self.stored_crafts.append (F16.status())
+        # elif x == aircraft_type(x):
+        #     self.stored_crafts.append (F35.status())
+        # return self.stored_crafts
 
 
 
+big_bill = Carrier()
+print(big_bill.health_point)
+big_bill.add_aircraft('F35')
+print(big_bill.stored_crafts)
