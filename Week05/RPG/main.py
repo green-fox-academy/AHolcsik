9ef5fcfe724a7070ab0b24651412de05f4c23d3a
AHolcsik <annamaria.holcsik@gmail.com>
2017-10-05 14:38:29 +0200

    refractor(RPG): map drawing works yet again

--- Week05/RPG/main.py
+++ Week05/RPG/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-# from view import 
+from view import Map 
 # from entity import 
 
 class Game(object):
@@ -9,37 +9,39 @@ class Game(object):
         canvas_height = 720
         canvas_width = 780
         canvas = Canvas(root, width=canvas_width, height=canvas_height)
-        self.hero = Hero(canvas)
+        self.map = Map()
+        self.map.draw_map(canvas)
+        # self.hero = Hero(canvas)
         # self.hero.draw(0, 0)
-        self.skeleton = Skeleton(canvas)
-        self.boss = Boss(canvas)
+        # self.skeleton = Skeleton(canvas)
+        # self.boss = Boss(canvas)
         # self.skeleton_number = 3
         # self.spots = self.map.create_enemy_spots(self.skeleton_number + 1)
         # self.skeleton.draw(self.spots[:-1])
         # self.boss.draw(self.spots[-1])
 
-        def on_key_press(self, e):
-            if (e.keysym == 'Up') and is_in_border_floor(Hero.coord_x, Hero.coord_y -1) == True:
-                Hero.move(0,-1)
-                Hero.look = hero_up
-                Hero.change_look(hero_up)
-            elif (e.keysym == 'Down') and is_in_border_floor(Hero.coord_x, Hero.coord_y +1) == True:
-                Hero.move(0,1)
-                Hero.look = hero_down
-                Hero.change_look(hero_down)
-            elif (e.keysym == 'Left') and is_in_border_floor(Hero.coord_x -1, Hero.coord_y) == True:
-                Hero.move(-1,0)
-                Hero.look = hero_left
-                Hero.change_look(hero_left)  
-            elif (e.keysym == 'Right') and is_in_border_floor(Hero.coord_x +1, Hero.coord_y) == True:
-                Hero.move(1,0)
-                Hero.look = hero_right
-                Hero.change_look(hero_right)
+        # def on_key_press(self, e):
+        #     if (e.keysym == 'Up') and is_in_border_floor(Hero.coord_x, Hero.coord_y -1) == True:
+        #         Hero.move(0,-1)
+        #         Hero.look = hero_up
+        #         Hero.change_look(hero_up)
+        #     elif (e.keysym == 'Down') and is_in_border_floor(Hero.coord_x, Hero.coord_y +1) == True:
+        #         Hero.move(0,1)
+        #         Hero.look = hero_down
+        #         Hero.change_look(hero_down)
+        #     elif (e.keysym == 'Left') and is_in_border_floor(Hero.coord_x -1, Hero.coord_y) == True:
+        #         Hero.move(-1,0)
+        #         Hero.look = hero_left
+        #         Hero.change_look(hero_left)  
+        #     elif (e.keysym == 'Right') and is_in_border_floor(Hero.coord_x +1, Hero.coord_y) == True:
+        #         Hero.move(1,0)
+        #         Hero.look = hero_right
+        #         Hero.change_look(hero_right)
 
 
 
-        root.bind("<KeyPress>", self.on_key_press)
-        canvas.pack()
+        # root.bind("<KeyPress>", self.on_key_press)
+        canvas.pack(padx = 1, pady = 0.6)
         root.mainloop()
 
 
--- Week05/RPG/view.py
+++ Week05/RPG/view.py
@@ -1,13 +1,13 @@
 from tkinter import *
 
-class Map(Object):
+class Map():
 
     def __init__(self):
         self.floor_tile = floor = PhotoImage(file = 'floor.gif')
         self.wall_tile = wall = PhotoImage(file = 'wall.gif')
         self.tile_size = 72
         self.x = 0
-        self.x = 0
+        self.y = 0
         self.map_layout = [[0,0,0,1,0,1,0,0,0,0],
                            [0,0,0,1,0,1,0,1,1,0],
                            [0,1,1,1,0,1,0,1,1,0],
@@ -19,27 +19,16 @@ class Map(Object):
                            [0,1,1,1,0,0,0,0,0,0],
                            ]
 
-        # def floor_piece(x, y, size):
-        #     floo = canvas.create_image(x * self.size, y * self.size, self.image = floor)
-
-        # def wall_piece(x, y, size):
-        #     woo = canvas.create_image(x * size, y * size, image = wall)
-
-        def draw_map(self, canvas):
-            for column in range(len(self.map_layout)):
-                for row in range(len(self.map_layout[0])):
-                    if self.map_layout[column][row] == 0:
-                        canvas.create_image(x * self.size, y * self.size, image = self.floor_tile)
-                    elif map_layout[column][row] == 1:
-                        canvas.create_image(x * self.size, y * self.size, image = self.wall_tile)
-        
-
-
-
+    def draw_map(self, canvas):
+        for i, line in enumerate(self.map_layout):
+            for j, value in enumerate(line):
+                if str(value) == '0':
+                    canvas.create_image(self.tile_size / 2 + j * self.tile_size, self.tile_size / 2 + i * self.tile_size, image = self.floor_tile)
+                elif str(value) == '1':
+                    canvas.create_image(self.tile_size / 2 + j * self.tile_size, self.tile_size / 2 + i * self.tile_size, image = self.wall_tile)     
 
 # def is_in_border_floor(x, y):
 #     if 0 <= x <= 9 and 0 <= y <= 8:
 #         if map_layout[y][x] == 0:
 #             return True
-
-draw_whole_map(canvas)        
+       
