815e7d10e440df4391ffcda779b4e0596e3a378c
AHolcsik <annamaria.holcsik@gmail.com>
2017-10-26 14:10:01 +0200

    feat(query_selector.js): exercise done

---
+++ Week07/Day4/query_selector1/query_selector1.js
@@ -0,0 +1,40 @@
+// <!-- You can work in the html or in a separate js file. Like:
+// <script>
+//   1. store the element that says 'The King' in the 'king' variable.
+//   console.log it.
+//   2. store the element that contains the text 'The Conceited Man'
+//   in the 'conceited' variable.
+//   show the result in an 'alert' window.
+//   3. store 'The Businessman' and 'The Lamplighter'
+//   in the 'businessLamp' variable.
+//   console.log each of them.
+//   4. store 'The King' and 'The Conceited Man'
+//   in the 'conceitedKing' variable.
+//   alert them one by one.
+//   5. store 'The King', 'The Conceited Man' and 'The Lamplighter'
+//   in the 'noBusiness' variable.
+//   console.log each of them.
+//   6. store 'The Businessman' in the 'allBizniss' variable.
+//   show the result in an 'alert' window.
+// </script>
+// OR
+
+let king = document.getElementById('b325')
+let conceited = document.querySelector('.b326')
+let businessLamp = document.querySelectorAll('.big')
+let conceitedKing = document.querySelectorAll('#b325, .b326')
+let noBusiness = document.querySelectorAll('#b325, .b326, .b329')
+let allBizniss = document.querySelector('.asteroid.big')
+
+console.log(king)
+console.log(conceited)
+console.log(businessLamp[0])
+console.log(businessLamp[1])
+console.log(conceitedKing[0])
+console.log(conceitedKing[1])
+console.log(noBusiness[0])
+console.log(noBusiness[1])
+console.log(noBusiness[2])
+console.log(allBizniss)
+
+
