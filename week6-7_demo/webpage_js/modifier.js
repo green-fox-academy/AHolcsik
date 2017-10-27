962c05f38a8fd318509c8076be87cdc0b2eea3e3
AHolcsik <annamaria.holcsik@gmail.com>
2017-10-27 14:15:26 +0200

    feat(demo): navigation bar set to be responsive to position of window

--- week6-7_demo/webpage_js/modifier.js
+++ week6-7_demo/webpage_js/modifier.js
@@ -1,11 +1,23 @@
-var wrap = ("#wrap");
 
-wrap.on("scroll", function(e) {
-    
-  if (this.scrollTop > 147) {
-    wrap.addClass("fix-search");
-  } else {
-    wrap.removeClass("fix-search");
+// window.addEventListener('scroll', tellMe)
+
+
+// function tellMe () {
+//   if (window.scrollY > 300) {
+//     console.log('O MY GAWD');
+//   }
+// }
+
+let nav = document.getElementsByTagName('nav')[0];
+
+window.addEventListener('scroll', makeFixed)
+
+function makeFixed () {
+  if (window.scrollY > 450) {
+    nav.setAttribute ('class', 'fixed');
+  } else if (window.scrollY < 450) {
+    nav.removeAttribute ('class', 'fixed')
   }
-  
-});
+}
+
+
