865d0806566d39b2ea585df43f4ae4b8ef960782
AHolcsik <annamaria.holcsik@gmail.com>
2017-10-30 15:57:13 +0100

    feat(sharpie.js): new sharpie added

---
+++ Week08/Day1/sharpie.js
@@ -0,0 +1,37 @@
+// Create a Sharpie constructor function
+
+// We should know about each sharpie:
+// color (which should be a string)
+// width (which will be a number)
+// inkAmount (another number)
+// When instantiating a Sharpie, we need to specify the color and the width
+// Every sharpie is created with a default 100 as inkAmount
+// We can use() the sharpie objects
+// which decreases inkAmount by the width
+// Write a loop that consumes all the sharpie's ink.
+// Make sure your loop works with any inkAmount, so your code figures out when it's out of ink.
+
+class sharpie {
+
+    constructor(color, width) {
+        this.color = color;
+        this.width = width;
+        this.inkAmount = 100;
+    }
+
+    use() {
+        this.inkAmount -= this.width;
+        return this.inkAmount;
+        console.log(this.inkAmount)
+    }
+
+    throwAway() {
+        if (this.inkAmount === 0) {
+            console.log(this.color + ' sharpie ran out of ink :c')
+        }
+    }
+}
+
+let peachpuff = new sharpie ('peachpuff', 20);
+peachpuff.use.bind(sharpie)
+console.log(peachpuff.inkAmount)
