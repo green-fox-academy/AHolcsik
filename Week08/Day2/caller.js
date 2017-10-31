915fb049533dfab9814d1c254c748f7af65a32c4
AHolcsik <annamaria.holcsik@gmail.com>
2017-10-31 16:59:29 +0100

    feat(caller.js): exercise done

---
+++ Week08/Day2/caller.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Implement the selectLastEvenNumber function that takes an array and callback,
+// it should call the callback immediately with the last even number on the array
+
+function selectLastEvenNumber(array, callback) {
+    let number = 0;
+    for (let i = 0; i <= array.length; i++) {
+        if (array[i] % 2 == 0) {
+            number = array[i];
+        }
+    }
+    callback(number)
+}
+
+function printNumber(num) {
+  console.log(num);
+}
+
+selectLastEvenNumber([2, 5, 7, 8, 9, 11], printNumber); // should print 8
