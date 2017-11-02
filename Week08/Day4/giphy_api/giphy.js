9e3dada047c01362e95db814f5350596021cb8ef
AHolcsik <annamaria.holcsik@gmail.com>
2017-11-02 13:58:32 +0100

    feat(giphy): base structure added, gifs are yet to be displayed

---
+++ Week08/Day4/giphy_api/giphy.js
@@ -0,0 +1,27 @@
+'use strict'
+
+
+let displayDiv = document.querySelector ('img')
+
+function getGif (callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=qgbcFlGRFhawbL3ndpdr6PXddqaDv7DF&q=gravity falls&limit=16&offset=0&rating=PG-13&lang=en');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            let Gifs = JSON.parse(xhr.responseText)
+            callback (Gifs)
+        }
+    }
+    xhr.send()
+}
+
+function displayGif(gifData) {
+    let toDisplay = gifData.data[0].images
+    console.log(toDisplay)
+    // displayDiv.setAttribute('src', toDisplay)
+}
+
+
+window.onload = function () {
+    getGif (displayGif);
+}
