5aba23b11b6c11d682415a8748bbdaa4be83dc57
AHolcsik <annamaria.holcsik@gmail.com>
2017-11-03 13:46:44 +0100

    feat(r/space): request posted in content

--- Week08/Day5/space.js
+++ Week08/Day5/space.js
@@ -1,5 +1,7 @@
 'use strict'
 
+let contentDiv = document.getElementsByClassName('content')[0]
+
 function getPosts (callback) {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://cors-anywhere.herokuapp.com/http://secure-reddit.herokuapp.com/posts')
@@ -10,7 +12,19 @@ function getPosts (callback) {
 }
 
 function displayData(data) {
-
+    let newUl = document.createElement('ul')
+    let newTitle = document.createElement('li')
+    let newUser = document.createElement('li')
+    // let newDate = document.createElement('li')
+    newTitle.textContent = data.title
+    newUser.textContent = data.user
+    // newDate.textContent = articleData.pub_date
+    console.log(newTitle)
+    console.log(newUser)
+    contentDiv.appendChild(newUl)
+    newUl.appendChild(newTitle)
+    newUl.appendChild(newUser)
+    // newUl.appendChild(newDate)
     console.log(data)
 }
 
