f2092034c8cd71a245d9ffef90e4bc8826ce7fe4
AHolcsik <annamaria.holcsik@gmail.com>
2017-11-08 15:56:37 +0100

    feat(doubling): exercise done

--- Week09/Day3/Frontend_project/backend.js
+++ Week09/Day3/Frontend_project/backend.js
@@ -2,10 +2,30 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function (req, res){
+app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 })
 
+app.get('/doubling', function(req,res){
+    var toDouble = req.query.input
+    var parsedToDouble = parseInt(toDouble)
+    if (!parsedToDouble){
+        res.send (
+            {
+                "error": "Please provide an input!"
+              }
+    )}
+    else {
+        res.send (
+            {
+                "received": parsedToDouble,
+                "result": parsedToDouble * 2
+              }
+        )
+    }
+})
+
+express.json.type = "application/json"
 app.use('/assets', express.static('assets'))
 
-app.listen(3000);
+app.listen(8080);
