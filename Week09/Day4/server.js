bc159f2d7de787d8acf01f08b10e13a13e0324f3
AHolcsik <annamaria.holcsik@gmail.com>
2017-11-09 15:50:47 +0100

    refract(bookstore): cleanup in server.js

--- Week09/Day4/server.js
+++ Week09/Day4/server.js
@@ -1,24 +1,24 @@
 'use strict'
 
-const mysql = require("mysql");
+const mysql = require('mysql');
 const express = require('express');
 const app = express();
 
 app.use(express.json())
 
 var conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "imanoodle",
-  database: "bookstore"
+  host: 'localhost',
+  user: 'root',
+  password: 'imanoodle',
+  database: 'bookstore'
 });
 
 conn.connect(function(err){
   if(err){
-    console.log("Error connecting to Db");
+    console.log('Error connecting to database oooh');
     return;
   }
-  console.log("Connection established");
+  console.log('Connection established woooo');
 });
 
 app.listen(3000, function() {
