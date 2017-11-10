c66cc2398011096eae0de08d9f26642978dd903e
AHolcsik <annamaria.holcsik@gmail.com>
2017-11-10 11:24:46 +0100

    feat(demo): testing for presentation

--- Week08/Day4/giphy_api/giphy.js
+++ Week08/Day4/giphy_api/giphy.js
@@ -2,7 +2,7 @@
 
 function getGif (callback) {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=qgbcFlGRFhawbL3ndpdr6PXddqaDv7DF&q=gravity falls&limit=16&offset=0&rating=PG-13&lang=en');
+    xhr.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=qgbcFlGRFhawbL3ndpdr6PXddqaDv7DF&q=gravity falls, bill cipher&limit=16&offset=0&rating=PG-13&lang=en');
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
             let Gifs = JSON.parse(xhr.responseText)
