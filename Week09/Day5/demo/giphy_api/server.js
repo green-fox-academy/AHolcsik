06193db052c924e638411c4b70e6d4defb02670a
AHolcsik <annamaria.holcsik@gmail.com>
2017-11-10 11:44:14 +0100

    fix(demo): connection between server.js and giphy.html fixed

--- Week09/Day5/demo/giphy_api/server.js
+++ Week09/Day5/demo/giphy_api/server.js
@@ -4,15 +4,15 @@ const express = require('express');
 const app = express();
 
 app.use(express.json())
-// app.use('/assets', express.static('./assets'));
+app.use('/assets', express.static('./assets'));
 
 
 app.get('/', function(req, res) {
     console.log('something')
-    res.sendFile(__dirname + '/index.html');    
+    res.sendFile(__dirname + '/giphy.html');    
 });
 
-// app.get('/bookstore', function(req, res) {
+// app.get('/', function(req, res) {
 //     conn.query('SELECT book_name FROM book_mast;', function(err, result) {
 //         if(err) {
 //             console.log(err.toString());
@@ -23,7 +23,7 @@ app.get('/', function(req, res) {
 
 
 
-app.listen(3000, function() {
-    console.log('server is up on port 3000, good to go!')
+app.listen(8080, function() {
+    console.log('server is up on port 8080, good to go!')
 })
 
