71c0559d559d985678b3ebf8e3ca1cecda5370c8
AHolcsik <annamaria.holcsik@gmail.com>
2017-11-14 17:34:59 +0100

    fix(fox_player): connection between files working

--- Week10/fox_player/server.js
+++ Week10/fox_player/server.js
@@ -3,13 +3,13 @@
 const express = require('express');
 const app = express();
 
-// app.use(express.json())
-// app.use('/assets', express.static('./assets'));
+app.use(express.json())
+app.use('/', express.static('./'))
 
 
-// app.get('/', function(req, res) {
-//     res.sendFile(__dirname + '/giphy.html');    
-// });
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/fox_index.html');    
+});
 
 app.listen(8080, function() {
     console.log('server is up on port 8080, good to go!')
