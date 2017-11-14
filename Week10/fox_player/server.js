7bac74bd2f28d7fc51ebb0423191c83c0db0d0e1
AHolcsik <annamaria.holcsik@gmail.com>
2017-11-14 17:06:35 +0100

    feat(fox_player): server added, express installed

---
+++ Week10/fox_player/server.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+
+// app.use(express.json())
+// app.use('/assets', express.static('./assets'));
+
+
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/giphy.html');    
+// });
+
+app.listen(8080, function() {
+    console.log('server is up on port 8080, good to go!')
+})
+
