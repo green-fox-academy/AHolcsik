77ac5c5869c8449508df63576925230f69b81d42
AHolcsik <annamaria.holcsik@gmail.com>
2017-11-14 18:22:15 +0100

    feat(fox_player): testing for display

--- Week10/fox_player/assets/controller.js
+++ Week10/fox_player/assets/controller.js
@@ -2,22 +2,24 @@
 
 const playlistContainer = document.querySelector('.playlists')
 const trackContainer = document.querySelector('.tracks')
-const listOplaylists = ['All tracks', 'Favourites']
-const listOtracks = ['MSI - Bitches', 'Turmion Katilot - Pyrun nyrkki', 'Bloodhound Gang - Um Tiss Um Tiss Um Tiss', 'Britney Spears - Oops I did it again']
+// const listOplaylists = ['All tracks', 'Favourites']
+// const listOtracks = ['MSI - Bitches', 'Turmion Katilot - Pyrun nyrkki', 'Bloodhound Gang - Um Tiss Um Tiss Um Tiss', 'Britney Spears - Oops I did it again']
 
-let addPlaylist = function() {
-    listOplaylists.forEach(function(playlist){
-        let them = document.createElement('li')
-        them.innerHTML = playlist
-        playlistContainer.appendChild(them)
-    })
-}()
+// let addPlaylist = function() {
+//     listOplaylists.forEach(function(playlist){
+//         let them = document.createElement('li')
+//         them.innerHTML = playlist
+//         playlistContainer.appendChild(them)
+//     })
+// }()
 
 let addTracks = function() {
-    listOtracks.forEach(function(track){
+    Tracks.forEach(function(track){
         let element = document.createElement('li')
         element.innerHTML = track
         console.log(element)
         trackContainer.appendChild(element)
     })
 }()
+
+console.log(Tracks)
