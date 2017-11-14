a23cfddf9d90e0271454a9a5944965341ecf74ac
AHolcsik <annamaria.holcsik@gmail.com>
2017-11-14 18:05:25 +0100

    fix(fox_player): files moved to assets

--- Week10/fox_player/server.js
+++ Week10/fox_player/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json())
-app.use('/', express.static('./'))
+app.use('/', express.static('./assets'))
 
 
 app.get('/', function(req, res) {
