86409ee16f2ac89670fcac94d786eb0b6b3a46c0
AHolcsik <annamaria.holcsik@gmail.com>
2017-11-15 12:16:36 +0100

    feat(fox_player): delete button added to playlist elements

--- Week10/fox_player/assets/Playlist.js
+++ Week10/fox_player/assets/Playlist.js
@@ -1,12 +1,20 @@
 'use strickt'
 
 const playlistContainer = document.querySelector('.playlists')
-const listOplaylists = ['All tracks', 'Favourites']
+const listOplaylists = ['All tracks', 'Favourites', 'Worst of the 90s']
 
 let addPlaylist = function() {
     listOplaylists.forEach(function(playlist){
-        let them = document.createElement('li')
-        them.innerHTML = playlist
-        playlistContainer.appendChild(them)
+        let contentPlaylist = `${playlist}`
+        let Playlist = document.createElement('li')
+        // dinamic variable will decide this part
+        if (playlist !== 'All tracks', 'Favourites') {
+            let button = `<button></button>`
+            Playlist.innerHTML = contentPlaylist + button
+        }
+        else {
+            Playlist.innerHTML = contentPlaylist
+        }
+        playlistContainer.appendChild(Playlist)
     })
 }()
